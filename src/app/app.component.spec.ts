3a3997aa8758176df5171c9be1eebd8630ba6c25
Jason <extremewinds@gmail.com>
2025-08-30 23:43:51 +0200

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AngularGitHubPages' title`, () => {
+  it(`should have the 'ColourGallery' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularGitHubPages');
+    expect(app.title).toEqual('ColourGallery');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularGitHubPages');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ColourGallery');
   });
 });
